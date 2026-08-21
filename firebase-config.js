506736de7670e433739fce1ccdc2df2b00bc1659
atrambrother766-lab <atrambrother766@gmail.com>
2026-08-21 13:08:01 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDdupYBQHUpVnkMDz5OmNvnCD0SjqMZenU",
+  authDomain: "wedding-invitation-520a3.firebaseapp.com",
+  databaseURL: "https://wedding-invitation-520a3-default-rtdb.firebaseio.com",
+  projectId: "wedding-invitation-520a3",
+  storageBucket: "wedding-invitation-520a3.firebasestorage.app",
+  messagingSenderId: "1036590091558",
+  appId: "1:1036590091558:web:78ed188d0d5a9ff5a505e7"
+};
---
+++ sync-config.js
@@ -0,0 +1,4 @@
+window.WEDDING_SYNC_CONFIG = {
+  databaseUrl: 'https://wedding-invitation-520a3-default-rtdb.firebaseio.com/',
+  invitationId: 'akash-vaishnavi-2026'
+};
